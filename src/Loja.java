5bda7833632f34f18320b3a060a8dc58201580c1
Luca <paesberaldiluca@gmail.com>
2024-04-27 16:05:38 -0300

    Criei a função de pagar os salários dos funcionários. A função funciona checando o saldo disponível da loja primeiro, a partir disso checa se é suficiente para depositar o salário de todos os funcionários. Caso seja, ele faz o deposito. Caso não, a função entra em um for que deposita para o número possível de funcionários, printando aqueles que não receberam.

--- src/Loja.java
+++ src/Loja.java
@@ -8,4 +8,33 @@ class Loja {
         this.conta = conta;
         this.funcionarios = funcionarios;
     }
+
+    public synchronized void pagarSalarios() {
+        double saldoDisponivel = conta.getSaldo();
+        double totalSalarios = funcionarios.length * Funcionario.SALARIO;
+
+        // caso o saldo é suficiente para o deposito do salário de TODOS os funcionários
+        if (saldoDisponivel >= totalSalarios) {
+            for (Funcionario funcionario : funcionarios) {
+                banco.controleTransferencia(Funcionario.SALARIO, funcionario.getContaSal(), conta);
+                funcionario.investir();
+                System.out.println("Salário pago a " + funcionario.getName());
+            }
+        // caso o saldo não é suficiente para o deposito do salário de TODOS os funcionários
+        } else {
+            System.out.println("A loja não possui saldo suficiente para pagar todos os salários.");
+
+            // for para tentar depositar o salário para o número possível de funcionários
+            for (Funcionario funcionario : funcionarios) {
+                if (saldoDisponivel >= Funcionario.SALARIO) {
+                    banco.controleTransferencia(Funcionario.SALARIO, funcionario.getContaSal(), conta);
+                    funcionario.investir();
+                    System.out.println("Salário pago a " + funcionario.getName());
+                    saldoDisponivel -= Funcionario.SALARIO;
+                } else {
+                    System.out.println("Saldo insuficiente para pagar o salário de " + funcionario.getName());
+                }
+            }
+        }
+    }
 }
