aa9f725802e58ac37d1ad78160a025d3e4026da2
Luca <paesberaldiluca@gmail.com>
2024-04-27 14:53:01 -0300

    Criei método de transferência no banco com os devidos controles de lock.

--- src/Banco.java
+++ src/Banco.java
@@ -1,2 +1,22 @@
-public class Banco {
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Banco {
+    private Lock lock = new ReentrantLock();
+
+    public synchronized void controleTransferencia(double valor, Conta origem, Conta destino) {
+        lock.lock();
+        try {
+            if (origem.getSaldo() >= valor) {
+                origem.sacar(valor);
+                destino.depositar(valor);
+                System.out.println("Transferência de R$" + valor + " realizada da conta de " + origem + " para a conta de " + destino + ".");
+            } else {
+                System.out.println("Saldo insuficiente!\n" + origem + " necessita de mais R$" + (valor - origem.getSaldo()) + " para realizar essa transferência.");
+            }
+        }
+        finally {
+            lock.unlock();
+        }
+    }
 }
