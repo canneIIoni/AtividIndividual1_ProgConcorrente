754ab600191f2204dfa709af8853b119fb10d301
Luca <paesberaldiluca@gmail.com>
2024-04-27 16:19:50 -0300

    Criei as propriedades básicas da classe Cliente, entre essas os valores das compras que alternam entre 100.0 e 200.0.

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,12 @@
-public class Cliente {
+class Cliente extends Thread {
+    private Banco banco;
+    private Conta conta;
+    private Loja[] lojas;
+    private static final double[] valorCompras = {100.0, 200.0}; // Enunciado pede compras de 100.0 ou 200.0
+
+    public Cliente(Conta conta, Loja[] lojas, Banco banco) {
+        this.banco = banco;
+        this.conta = conta;
+        this.lojas = lojas;
+    }
 }
