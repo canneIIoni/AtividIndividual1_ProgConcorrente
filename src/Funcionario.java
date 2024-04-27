1b89772b1e813f76165add18b41294684b2e4b36
Luca <paesberaldiluca@gmail.com>
2024-04-27 13:37:12 -0300

    Defini o salário fixo, criei a conta do salário e a conta dos investimentos.

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,2 +1,10 @@
-public class Funcionario {
+class Funcionario extends Thread {
+    private Conta contaSal;
+    private Conta contaInvest;
+    public static final double SALARIO = 1400.0;
+
+    public Funcionario(Conta contaSal, Conta contaInvest) {
+        this.contaSal = contaSal;
+        this.contaInvest = contaInvest;
+    }
 }
