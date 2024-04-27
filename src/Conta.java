b03d00006de92169a29425176bd19ed7a0c1e2ee
Luca <paesberaldiluca@gmail.com>
2024-04-27 12:18:03 -0300

    Criei as funções de ver saldo, sacar, depositar e transferir para outra conta na classe Conta.

--- src/Conta.java
+++ src/Conta.java
@@ -1,2 +1,25 @@
-public class Conta {
+class Conta {
+    private double saldo;
+
+    public Conta(double valorInicial) {
+        this.saldo = valorInicial;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void transferir(Conta destino, double valor) {
+        // Saca valor da Conta atual e deposita valor na Conta destino.
+        sacar(valor);
+        destino.depositar(valor);
+    }
 }
